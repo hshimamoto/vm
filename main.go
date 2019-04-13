805c921ba74058358e77b282e1372da8ac8f5d8d
Hiroshi Shimamoto <hshimamoto@users.noreply.github.com>
2019-04-13 23:01:18 +0000

    add vm list command

--- main.go
+++ main.go
@@ -6,10 +6,15 @@
 package main
 
 import (
+    "bytes"
+    "io/ioutil"
     "os"
     "fmt"
+    "strings"
 
     "github.com/hshimamoto/vm/qemu"
+    // for list
+    "github.com/mitchellh/go-ps"
 )
 
 func launch(opts []string) {
@@ -26,6 +31,41 @@ func launch(opts []string) {
     }
 }
 
+func list(opts []string) {
+    processes, err := ps.Processes()
+    if err != nil {
+	return
+    }
+    for _, p := range processes {
+	pid := p.Pid()
+	bin := p.Executable()
+	if strings.Index(bin, "qemu") == -1 {
+	    continue
+	}
+	//fmt.Printf("%d: %s\n", pid, bin)
+	cmdline := fmt.Sprintf("/proc/%d/cmdline", pid)
+	f, err := os. Open(cmdline)
+	if err != nil {
+	    // disappeared?
+	    continue
+	}
+	data, err := ioutil.ReadAll(f)
+	f.Close()
+	name := ""
+	disp := ""
+	args := bytes.Split(data, []byte("\x00"))
+	for i, arg := range args {
+	    arg := string(arg)
+	    if arg == "-name" {
+		name = string(args[i+1])
+	    } else if arg == "-display" {
+		disp = string(args[i+1])
+	    }
+	}
+	fmt.Printf("%d %s %s\n", pid, name, disp)
+    }
+}
+
 func main() {
     if len(os.Args) == 1 {
 	os.Exit(1)
@@ -34,5 +74,7 @@ func main() {
     fmt.Println(subcmd)
     if subcmd == "launch" {
 	launch(os.Args[2:])
+    } else if subcmd == "list" {
+	list(os.Args[2:])
     }
 }
