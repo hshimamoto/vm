58a2b719969ac93fe5411918c259b850ee8052e0
Hiroshi Shimamoto <hshimamoto@users.noreply.github.com>
2019-05-10 22:08:51 +0000

    qemu: disable tap scripts

--- qemu/network.go
+++ qemu/network.go
@@ -62,6 +62,9 @@ func (n *network)value() string {
     case "tap":
 	v = push(v, "ifname", n.ifname)
 	v = push(v, "fd", n.nsnwtapfd)
+	// never use scripts
+	v = push(v, "script", "no")
+	v = push(v, "downscript", "no")
     default:
 	// unknown
     }
